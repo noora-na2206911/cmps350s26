68d1c54540371c759fc45f8f3b24398b22780928
noora-na2206911 <narjd2211@gmail.com>
2026-05-06 13:15:22 +0300

    nav

---
+++ Final Exam/Final Exam/volunteerlog-app/app/components/NavBar.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+export function navBarHTML() {
+    return (
+        <nav class="navbar">
+            <Link href="/client/index.html" class="brand">VolunteerLog</Link>
+            <ul class="nav-links">
+                <li>< Link href="/client/index.html">My Activities</ Link></li>
+                <li>< Link href="/client/pages/activity-form.html">Add Activity</ Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+   
