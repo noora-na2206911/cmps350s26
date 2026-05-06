33a82fe5638dcc22d2759d454475521e554e6a33
noora-na2206911 <narjd2211@gmail.com>
2026-05-06 14:32:51 +0300

    Update layout.js

--- Final Exam/Final Exam/volunteerlog-app/app/layout.js
+++ Final Exam/Final Exam/volunteerlog-app/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import NavBar from "@/app/components/NavBar.jsx";
 
 export const metadata = {
     title: "VolunteerLog",
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
+                <NavBar />
                 {children}
             </body>
         </html>
